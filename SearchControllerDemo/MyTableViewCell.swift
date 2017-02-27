150b601ac8422ef1139d013de68aeb95d53bea69
Sebastian Strus <sebastian.strus1@gmail.com>
2017-02-27 08:00:18 +0100

    first commit

---
+++ SearchControllerDemo/MyTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  MyTableViewCell.swift
+//  SearchControllerDemo
+//
+//  Created by Sebastian Strus on 2017-02-24.
+//  Copyright © 2017 Sebastian Strus. All rights reserved.
+//
+
+import UIKit
+
+class MyTableViewCell: UITableViewCell {
+
+    
+    var fruit : String?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
